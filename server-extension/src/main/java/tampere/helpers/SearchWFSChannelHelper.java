452fafc65ca3eb1df88b96df319f9e89be6f63ff
Tuomas Ruohonen <tuomas.ruohonen@dimenteq.fi>
2015-08-14 10:21:14 +0300

    Modified WFS search for publisher search

--- server-extension/src/main/java/tampere/helpers/SearchWFSChannelHelper.java
+++ server-extension/src/main/java/tampere/helpers/SearchWFSChannelHelper.java
@@ -50,6 +50,22 @@ public class SearchWFSChannelHelper {
 	}
 	
 	/**
+	 * Get WFS channels id's
+	 * @return JSONArray
+	 */
+	public static JSONArray getChannelsIds() throws JSONException{
+		JSONArray job = new JSONArray();
+		List<WFSSearchChannelsConfiguration> channels = channelService.findChannels();
+		
+		for(int i=0;i<channels.size();i++){
+			WFSSearchChannelsConfiguration channel = channels.get(i);
+			job.put(channel.getId());
+		}
+	   	 
+	   	return job;
+	}
+	
+	/**
 	 * Get channel by id
 	 * @param channelIds
 	 * @return
