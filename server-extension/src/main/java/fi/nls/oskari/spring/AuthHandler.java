7d7e32239e1ce4c0f8571045aa349b2e3732e0c0
Sami Mäkinen <zakarfin@gmail.com>
2019-03-10 23:37:33 +0200

    Use redirectview directly and construct url

--- server-extension/src/main/java/fi/nls/oskari/spring/AuthHandler.java
+++ server-extension/src/main/java/fi/nls/oskari/spring/AuthHandler.java
@@ -4,10 +4,12 @@ import fi.nls.oskari.control.ActionParameters;
 import fi.nls.oskari.log.LogFactory;
 import fi.nls.oskari.log.Logger;
 import fi.nls.oskari.spring.extension.OskariParam;
+import fi.nls.oskari.util.PropertyUtil;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Profile("preauth")
 @Controller
@@ -17,9 +19,11 @@ public class AuthHandler {
     private final static Logger LOG = LogFactory.getLogger(AuthHandler.class);
 
     @RequestMapping
-    public ModelAndView index(@OskariParam ActionParameters params) throws Exception {
-        LOG.info("User logged in:", params.getRequest().getHeader("auth-email"));
-        return new ModelAndView("redirect:/");
+    public RedirectView index(@OskariParam ActionParameters params,
+                              RedirectAttributes attributes) {
+        LOG.info("User logged in:", params.getRequest().getHeader("X_EMAIL"));
+        String url = PropertyUtil.get("oskari.domain") + PropertyUtil.get("oskari.map.url");
+        return new RedirectView (url);
     }
 
 }
