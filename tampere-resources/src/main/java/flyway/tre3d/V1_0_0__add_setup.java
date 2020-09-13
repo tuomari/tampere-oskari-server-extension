5945ed84efe0ec94d8d5f94249eac3f9168f1c10
Sami Mäkinen <zakarfin@gmail.com>
2020-09-13 23:05:44 +0300

    3D buildings tileset

--- tampere-resources/src/main/java/flyway/tre3d/V1_0_0__add_setup.java
+++ tampere-resources/src/main/java/flyway/tre3d/V1_0_0__add_setup.java
@@ -1,5 +1,6 @@
 package flyway.tre3d;
 
+import fi.nls.oskari.db.LayerHelper;
 import fi.nls.oskari.db.ViewHelper;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
@@ -7,6 +8,7 @@ import java.sql.Connection;
 
 public class V1_0_0__add_setup implements JdbcMigration {
     public void migrate(Connection connection) throws Exception {
+        LayerHelper.setupLayer("buildings3d.json");
         ViewHelper.insertView(connection, "geoportal3d.json");
     }
 }
